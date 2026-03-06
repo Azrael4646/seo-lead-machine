a6b353146d9ca6cc1627bfc47fb35b8ba2dee938
Azrael4646 <43372356+Azrael4646@users.noreply.github.com>
2026-03-06 11:08:37 +0200

    Update email_finder.py

--- email_finder.py
+++ email_finder.py
@@ -2,6 +2,12 @@ import re
 
 def find_email(html):
 
+    if not html:
+        return []
+
+    if not isinstance(html, str):
+        return []
+
     email_regex = r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+"
 
     emails = re.findall(email_regex, html.lower())
